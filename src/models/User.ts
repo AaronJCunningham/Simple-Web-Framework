0426cebee8a0dcc77f204f9d1b8ad233be502ed9
Aaron J Cunningham <aaronjosephcunningham@gmail.com>
2021-02-11 12:03:43 +0100

    4th

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,7 +1,8 @@
-import { Model } from "./Model";
-import { Attributes } from "./Attributes";
-import { ApiSync } from "./ApiSync";
-import { Eventing } from "./Eventing";
+import { Model } from './Model';
+import { Attributes } from './Attributes';
+import { ApiSync } from './ApiSync';
+import { Eventing } from './Eventing';
+import { Collection } from './Collection';
 
 export interface UserProps {
   id?: number;
@@ -9,7 +10,7 @@ export interface UserProps {
   age?: number;
 }
 
-const rootUrl = "http://localhost:3000/users";
+const rootUrl = 'http://localhost:3000/users';
 
 export class User extends Model<UserProps> {
   static buildUser(attrs: UserProps): User {
@@ -19,6 +20,15 @@ export class User extends Model<UserProps> {
       new ApiSync<UserProps>(rootUrl)
     );
   }
-}
 
-const user = User.buildUser({});
+  static buildUserCollection(): Collection<User, UserProps> {
+    return new Collection<User, UserProps>(rootUrl, (json: UserProps) =>
+      User.buildUser(json)
+    );
+  }
+
+  setRandomAge(): void {
+    const age = Math.round(Math.random() * 100);
+    this.set({ age });
+  }
+}
